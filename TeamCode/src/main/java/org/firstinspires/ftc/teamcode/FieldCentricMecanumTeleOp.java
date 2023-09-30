563d6325780e9918c2bd9d2ddbef42ed71b5d41a
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-09-30 09:41:16 -0500

    Testing bug fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -42,8 +42,8 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; //Y stick value is reversed
-            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y; //Y stick value is reversed
+            double x = -gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
             // This button choice was made so that it is hard to hit on accident,
