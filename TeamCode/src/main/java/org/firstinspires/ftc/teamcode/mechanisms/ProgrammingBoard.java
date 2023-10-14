31752c6a637f4f44005b25a4f97f6af59d9d58e1
LeoTemino2 <leot25826@isd273.org>
2023-10-14 11:23:22 -0500

    Attempt
    1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class ProgrammingBoard {
+    private DigitalChannel touchSensor;
+
+    public void init (HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+    }
+    public boolean getTouchSensorState() {
+        return touchSensor.getState();
+    }
+}
