538c33a9ee7a8f16c0843c57bb27e3badf82862f
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-10-28 12:08:02 -0500

    meep meep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByEncoder_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByEncoder_Linear.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.motors.NeveRest40Gearmotor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,9 +18,9 @@ public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
 
     private final ElapsedTime     runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     COUNTS_PER_MOTOR_REV    = 1 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 3.0 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 3.03 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double     DRIVE_SPEED             = 1.0;
@@ -27,7 +28,7 @@ public class RobotAutoDriveByEncoder_Linear extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
+        NeveRest40Gearmotor
         frontLeftMotor  = hardwareMap.get(DcMotor.class, "frontLeftMotor");
         frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
         backLeftMotor  = hardwareMap.get(DcMotor.class, "backLeftMotor");
