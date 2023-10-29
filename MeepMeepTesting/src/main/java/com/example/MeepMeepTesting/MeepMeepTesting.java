40f291a946e549e18702e3f8656fadb9b46e7643
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-10-29 13:19:30 -0500

    added the real duplicate code for the starting point

--- MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
@@ -1,25 +1,26 @@
 package com.example.MeepMeepTesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Pose2dKt;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(900);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 100, Math.toRadians(180), Math.toRadians(180), 200)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-10, 0, 61))
+                        drive.trajectorySequenceBuilder(new Pose2d( -10, 61))
+
                                 .turn(Math.toRadians(-90))
                                 .forward(25)
                                 .strafeLeft(45)
                                 .forward(24)
                                 .strafeLeft(23)
-                                .turn(Math.toRadians(90))
                                 .build()
                 );
 
@@ -30,5 +31,3 @@ public class MeepMeepTesting {
                 .start();
     }
 }
-
-
