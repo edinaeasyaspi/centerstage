ba97c394864c48c1ff48a7e70507bfb6c4a5745c
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-10-29 11:30:01 -0500

    updates

--- MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/MeepMeepTesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.MeepMeepTesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Pose2dKt;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,13 +12,14 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 100, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 100, Math.toRadians(180), Math.toRadians(180), 200)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d( -10, 61))
+
                                 .turn(Math.toRadians(-90))
                                 .forward(25)
                                 .strafeLeft(45)
-                                .forward(23)
+                                .forward(24)
                                 .strafeLeft(23)
                                 .build()
                 );
