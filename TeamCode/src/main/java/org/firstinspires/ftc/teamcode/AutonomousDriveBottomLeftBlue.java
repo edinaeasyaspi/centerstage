a681a7e4a96e9cb9c6bf79d6dee0f63b3e079365
Ratik-Jain <144185481+Ratik-Jain@users.noreply.github.com>
2023-11-07 19:05:37 -0600

    Updated how trajectory is built

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBottomLeftBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBottomLeftBlue.java
@@ -5,6 +5,7 @@ package org.firstinspires.ftc.teamcode;
         import com.acmerobotics.dashboard.config.Config;
         import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
         import com.acmerobotics.roadrunner.geometry.Pose2d;
+        import com.acmerobotics.roadrunner.trajectory.Trajectory;
         import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
         import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
         import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -34,26 +35,32 @@ public abstract class AutonomousDriveBottomLeftBlue extends LinearOpMode {
     public class TurnTest extends LinearOpMode {
         public double DISTANCE1 = 96;
 
-
-
-
         @Override
         public void runOpMode() throws InterruptedException {
             Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
             SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+            drive.turn(Math.toRadians(-90));
 
-            TrajectoryBuilder trajectory = drive.trajectoryBuilder(new Pose2d());
-            trajectory.strafeLeft(DISTANCE1);
+            Trajectory autonomousTrajectory = drive.trajectoryBuilder(new Pose2d())
+            .strafeLeft(25)
+            .forward(25)
+            .strafeLeft(25)
+            .back(25)
+            .strafeLeft(45)
+            .build();
 
-            trajectory.build();
+//            TrajectoryBuilder trajectory = drive.trajectoryBuilder(new Pose2d());
+//            trajectory.strafeLeft(DISTANCE1);
+//
+//            trajectory.build();
 
 
             waitForStart();
 
             if (isStopRequested()) return;
 
-            drive.followTrajectory(trajectory.build());
+            drive.followTrajectory(autonomousTrajectory);
 
             Pose2d poseEstimate = drive.getPoseEstimate();
             telemetry.addData("finalX", poseEstimate.getX());
