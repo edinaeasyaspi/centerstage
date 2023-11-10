ae3ccce2748c93cc188a319af2275421cd20acae
Ratik-Jain <144185481+Ratik-Jain@users.noreply.github.com>
2023-11-09 19:28:28 -0600

    CHanged the trajectory to use SplineTo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBottomLeftBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveBottomLeftBlue.java
@@ -5,6 +5,7 @@ package org.firstinspires.ftc.teamcode;
         import com.acmerobotics.dashboard.config.Config;
         import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
         import com.acmerobotics.roadrunner.geometry.Pose2d;
+        import com.acmerobotics.roadrunner.geometry.Vector2d;
         import com.acmerobotics.roadrunner.trajectory.Trajectory;
         import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
         import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -18,21 +19,7 @@ package org.firstinspires.ftc.teamcode;
  */
 @Config
 @Autonomous(group = "drive")
-public abstract class AutonomousDriveBottomLeftBlue extends LinearOpMode {
-
-
-    abstract class sleep {
-        // Declare an abstract sleep method
-        public void sleep() {
-            try {
-                Thread.sleep(1000); // Sleep for 1 second (1000 milliseconds)
-            } catch (InterruptedException e) {
-                //handle the interrupted exception if necessary
-            }
-        }
-    }
-
-    public class TurnTest extends LinearOpMode {
+public class AutonomousDriveBottomLeftBlue extends LinearOpMode {
         public double DISTANCE1 = 96;
 
         @Override
@@ -40,27 +27,32 @@ public abstract class AutonomousDriveBottomLeftBlue extends LinearOpMode {
             Telemetry telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
 
             SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-            drive.turn(Math.toRadians(-90));
+           // drive.turn(Math.toRadians(-90));
 
-            Trajectory autonomousTrajectory = drive.trajectoryBuilder(new Pose2d())
-            .strafeLeft(25)
-            .forward(25)
-            .strafeLeft(25)
-            .back(25)
-            .strafeLeft(45)
-            .build();
+            Trajectory autonomousTrajectory = drive.trajectoryBuilder(new Pose2d(-35,61))
+                    .splineTo(new Vector2d(-60,61), Math.toRadians(0))
+                    .splineTo(new Vector2d(-60,86), Math.toRadians(0))
+                    .splineTo(new Vector2d(-85,86), Math.toRadians(0))
+                    .splineTo(new Vector2d(-85,61), Math.toRadians(0))
+                    .splineTo(new Vector2d(-110,61), Math.toRadians(0))
+
+                    //.strafeLeft(25)
+                    // .forward(25)
+                    // .strafeLeft(25)
+                    // .back(25)
+                    //.strafeLeft(45)
+                    .build();
 
 //            TrajectoryBuilder trajectory = drive.trajectoryBuilder(new Pose2d());
 //            trajectory.strafeLeft(DISTANCE1);
-//
 //            trajectory.build();
 
 
             waitForStart();
 
-            if (isStopRequested()) return;
-
-            drive.followTrajectory(autonomousTrajectory);
+            while (opModeIsActive() && !isStopRequested()) {
+                drive.followTrajectory(autonomousTrajectory);
+            }
 
             Pose2d poseEstimate = drive.getPoseEstimate();
             telemetry.addData("finalX", poseEstimate.getX());
@@ -71,6 +63,6 @@ public abstract class AutonomousDriveBottomLeftBlue extends LinearOpMode {
             while (!isStopRequested() && opModeIsActive()) ;
         }
     }
-}
+//}
 
 
