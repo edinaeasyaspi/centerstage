211bee939eddb2850c9e1846f0ee4c272272c14a
Sunking2020 <144185841+Sunking2020@users.noreply.github.com>
2023-11-11 11:55:11 -0600

    Added servo code and programming board

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoGamepadOpMode.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ServoProgrammingBoard;
+
+@TeleOp()
+public class ServoGamepadOpMode extends OpMode {
+    ServoProgrammingBoard board = new ServoProgrammingBoard();
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a) {
+            board.setServoPosition(1.0);
+        }
+        else if(gamepad1.b){
+            board.setServoPosition(0.0);
+        }
+        else{
+            board.setServoPosition(0.5);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
